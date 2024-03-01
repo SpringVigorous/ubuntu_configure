eec178a029725fa53edf2e4e293764896c5d8609
SpringFlourish <spring_flourish@outlook.com>
2024-03-01 20:16:28 +0800

    jinga 配置生效

--- assist/python/config.py
+++ assist/python/config.py
@@ -1,47 +1,23 @@
 
-from dynaconf import Dynaconf,DjangoDynaconf,loaders 
+from dynaconf import Dynaconf
 import json
 
 
 # dynaconf -i config.settings list
 
-from jinja2 import Environment, FileSystemLoader
-import yaml
-
-# # 创建一个 Jinja2 环境用于渲染模板
-# jinja_env = Environment(loader=FileSystemLoader('.'))
-# def render_jinja2_template(filename):
-#     template = jinja_env.get_template(filename)
-#     rendered_config_str = template.render()  # 假设你已经正确注入了模板需要的环境变量等数据
-#     return rendered_config_str
-
-# class RenderedYamlLoader(loaders.yaml_loader):
-#     def load(self, filename, **kwargs):
-#         rendered_yaml_content = render_jinja2_template(filename)
-#         loaded_config = yaml.safe_load(rendered_yaml_content)  # 将渲染后的字符串转换为 Python 字典
-#         return loaded_config
-
-
-
-
-
-
-
 settings = Dynaconf(
     env_switcher="LAUNCH_ENV",
-    envvar_prefix="DYNACONF",
-    # settings_files=['config/settings.toml', 'config/.secrets.toml'],
+    # envvar_prefix="DYNACONF",
+
+    # settings_files=['config/settings.yaml','config/.secrets.yaml'],
     settings_files=['config/settings.yaml'],
     environments=True,
     # 环境变量前缀，环境变量导出方式 `export CONF_FOO=bar` 变成 `settings.foo == "bar"`
-    # envvar_prefix="CONFIG",
+    envvar_prefix="CONFIG",
     lowercase_read=True,
-    # loaders=[RenderedYamlLoader()],
-    # yaml_loader="safe_load",
 )
-# settings1 = DjangoDynaconf(__name__) # noqa
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-# `settings_files` = Load these files in the order.
+
+
 settings.setenv("config")
 
 
