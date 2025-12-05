abdadb7b3c84aa679783cb5d7df9ae15cb8f0ac3
huming <1107917658@qq.com>
2025-12-05 20:03:41 +0800

    修改日志更新等级

--- assist/python/base/xlsx_manager.py
+++ assist/python/base/xlsx_manager.py
@@ -124,10 +124,10 @@ class xlsx_manager(metaclass=abc.ABCMeta):
     @exception_decorator(error_state=False)
     def save(self,):
         #保存前处理
-        self.logger.update_time(UpdateTimeType.STAGE)
+        self.logger.update_time(UpdateTimeType.ALL)
         with self.logger.raii_target("保存前处理") as logger:
             self.before_save()
-            logger.info("完成",update_time_type=UpdateTimeType.STAGE)
+            logger.info("完成",update_time_type=UpdateTimeType.ALL)
 
 
 
@@ -136,7 +136,7 @@ class xlsx_manager(metaclass=abc.ABCMeta):
                 if not dfs: 
                     continue
                 self.save_dfs(xlsx_path,dfs)
-            logger.info("完成",update_time_type=UpdateTimeType.STAGE)
+            logger.info("完成",update_time_type=UpdateTimeType.ALL)
     @staticmethod
     #设置df的sheet_name
     def set_xlsx_path(df:pd.DataFrame,name:Any)->pd.DataFrame:
